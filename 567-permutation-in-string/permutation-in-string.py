e671f8ca79a9334a83984409ee3d9102cd9e7469
Bharath Raghuraman <br300307@gmail.com>
2026-03-24 21:25:45 +0530

    Time: 17 ms (65.38%) | Memory: 19.4 MB (74.96%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1)>len(s2):
+            return False
+        tup = [0 for i in range(26)]
+        fm = [0 for i in range(26)]
+        for i in s1:
+            tup[ord(i)-ord('a')] +=1
+        lm = len(s1)
+        for i in range(lm):
+            fm[ord(s2[i])-ord('a')] +=1
+        for i in range(len(s2)-lm+1):
+            if tup == fm:
+                return True
+            if i+lm>=len(s2):
+                break
+            fm[ord(s2[i])-ord("a")] -=1
+            fm[ord(s2[i+lm])-ord("a")] +=1
+        return False
