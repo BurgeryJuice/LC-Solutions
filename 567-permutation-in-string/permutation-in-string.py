069d2e7bf1cdd99ca8924f93f80dd5ff66a5e164
Bharath Raghuraman <br300307@gmail.com>
2026-03-24 21:28:36 +0530

    Time: 15 ms (82.25%) | Memory: 19.4 MB (42.16%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -5,15 +5,15 @@ class Solution:
         tup = [0 for i in range(26)]
         fm = [0 for i in range(26)]
         for i in s1:
-            tup[ord(i)-ord('a')] +=1
+            tup[ord(i)-97] +=1
         lm = len(s1)
         for i in range(lm):
-            fm[ord(s2[i])-ord('a')] +=1
+            fm[ord(s2[i])-97] +=1
         for i in range(len(s2)-lm+1):
             if tup == fm:
                 return True
             if i+lm>=len(s2):
                 break
-            fm[ord(s2[i])-ord("a")] -=1
-            fm[ord(s2[i+lm])-ord("a")] +=1
+            fm[ord(s2[i])-97] -=1
+            fm[ord(s2[i+lm])-97]+=1
         return False
