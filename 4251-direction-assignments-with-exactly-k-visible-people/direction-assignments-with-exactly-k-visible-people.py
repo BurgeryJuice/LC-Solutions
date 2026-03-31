a0bf5249dbe9f8a730f26ef364f7938802d74942
Bharath Raghuraman <br300307@gmail.com>
2026-03-31 12:28:14 +0530

    Time: 706 ms (36.85%) | Memory: 21.5 MB (48.42%) - LeetSync

--- 4251-direction-assignments-with-exactly-k-visible-people/direction-assignments-with-exactly-k-visible-people.py
+++ 4251-direction-assignments-with-exactly-k-visible-people/direction-assignments-with-exactly-k-visible-people.py
@@ -1,8 +1,7 @@
 import math as m 
 class Solution:
     def countVisiblePeople(self, n: int, pos: int, k: int) -> int:
-        pos = 0
-        pos+=2*(m.comb(n-1,k))
+        
 
 
-        return pos%((10**9)+7)
+        return (2*(m.comb(n-1,k)))%((10**9)+7)
